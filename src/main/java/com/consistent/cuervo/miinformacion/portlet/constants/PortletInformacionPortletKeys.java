ab7868d73a438aa83e18458e1cda550230cc3192
Bernardo Hernandez <bhernandez@consistent.com.mx>
2019-11-07 00:18:29 -0600

    Initial commit

---
+++ src/main/java/com/consistent/cuervo/miinformacion/portlet/constants/PortletInformacionPortletKeys.java
@@ -0,0 +1,10 @@
+package com.consistent.cuervo.miinformacion.portlet.constants;
+
+/**
+ * @author bernardohernadez
+ */
+public class PortletInformacionPortletKeys {
+
+	public static final String PortletInformacion = "portletinformacion";
+
+}
